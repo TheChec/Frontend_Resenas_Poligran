1e35e47a93b3aa1d2e84e6ac88b50a038a325825
TheChec <chechoosorio@hotmail.com>
2025-04-08 03:31:54 -0500

    frontend ready

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,153 @@
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { ProductService } from '../../../services/product.service';
+import { Product, Review } from '../../../models/product.model';
+import { UserLocalStorage } from '../../../models/user.model';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent implements OnInit {
+
+  user: UserLocalStorage = new UserLocalStorage();
+  products: Product[] = [];
+  selectProduct: Product = new Product();
+  stars = [
+    { cantidad: 1, nombre: '1 Estrella' },
+    { cantidad: 2, nombre: '2 Estrella' },
+    { cantidad: 3, nombre: '3 Estrella' },
+    { cantidad: 4, nombre: '4 Estrella' },
+    { cantidad: 5, nombre: '5 Estrella' }
+  ]
+  categorias = [
+    { id: 1, nombre: 'Tecnología' },
+    { id: 2, nombre: 'Cosméticos' },
+    { id: 3, nombre: 'Electrodomesticos' },
+    { id: 4, nombre: 'Decoraciones' }
+  ];
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.detectUser()
+    this.getProducts()
+  }
+
+  detectUser() {
+    const userStr = localStorage.getItem('user');
+    if (userStr) {
+      this.user = JSON.parse(userStr);
+    }
+  }
+  
+
+  getProducts(){
+    this.productService.getProducts().subscribe(res => {
+      if(res) this.products = res.data as Product[]
+    }, err => {
+      console.log(err.error.error)
+    })
+  }
+
+  toLogin() {
+    this.router.navigate(['/inicio_de_sesion'])
+  }
+
+  getByCategory(idCat: string) {
+    this.productService.getProductsByCategory(idCat).subscribe(res => {
+      if (res) {
+        Swal.fire({
+          icon: 'success',
+          title: '¡Muy bien!',
+          text: res.message,
+        });
+        this.products = res.data as Product[]
+      }
+    }, err => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error: ' + err.status,
+        text: err.error.error,
+      });
+    })
+  }
+  modalOpen(idModal: string, idBox: string, index: number) {
+    const modal = document.getElementById(idModal);
+    const box = document.getElementById(idBox);
+  
+    if (modal && box) {
+      modal.style.display = 'flex';
+      box.classList.add('animacion_modal');
+      document.body.classList.add('no-scroll'); // <--- evita movimiento del fondo
+      this.selectProduct = this.products[index];
+    }
+  }
+  
+  closeModal(idModal: string, idBox: string) {
+    const modal = document.getElementById(idModal);
+    const box = document.getElementById(idBox);
+  
+    if (modal && box) {
+      box.classList.remove('animacion_modal');
+      box.classList.add('ocultar_modal');
+  
+      setTimeout(() => {
+        modal.style.display = 'none';
+        box.classList.remove('ocultar_modal');
+        document.body.classList.remove('no-scroll'); // <--- vuelve a habilitar scroll
+      }, 300);
+    }
+  }
+  getNombreCategoria(categoriaId: any): string {
+    const id = parseInt(categoriaId, 10); // Por si viene como string
+    const categoria = this.categorias.find(c => c.id === id);
+    return categoria ? categoria.nombre : 'Categoría desconocida';
+  }
+  
+
+
+  formAddReview = new FormGroup({
+    comment: new FormControl('', [Validators.required]),
+    stars: new FormControl(0, [Validators.required]),
+  })
+
+  putReview(){
+    const formValues = this.formAddReview.value
+
+    let review:Review = new Review(
+      this.user._id,
+      this.user.name+" "+this.user.lastName,
+      this.user.img || '',
+      formValues.comment || '',
+      formValues.stars || 0
+    )
+
+    this.productService.putReview(this.selectProduct._id || '', review).subscribe(res => {
+      if(res){
+        Swal.fire({
+          icon: 'success',
+          title: '¡Muy bien!',
+          text: res.message,
+        });
+        window.location.reload()
+      }
+    }, err => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error: ' + err.status,
+        text: err.error.error,
+      });
+    })
+  }
+
+
+}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,184 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+import { LoginService } from '../../../services/login.service';
+import { UserService } from '../../../services/user.service';
+import { UserLogin, UserLocalStorage, User } from '../../../models/user.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  constructor( 
+    private authService: LoginService,
+    private userService: UserService
+  ){}
+
+  ngOnInit(): void {
+    
+  }
+
+  formRegister = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    repassword: new FormControl('', [Validators.required]),
+  })
+
+  formLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  signIn() {
+    const formValues = this.formRegister.value;
+    const regexPassword = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{5,}$/;
+    const inputFile = document.getElementById('input_file') as HTMLInputElement;
+    const file = inputFile?.files?.[0];
+
+     if (!file) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Debes cargar un archivo',
+      });
+      return;
+    }
+  
+    if (formValues.password !== formValues.repassword) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Las contraseñas deben ser iguales',
+      });
+      return;
+    }
+  
+    if (!regexPassword.test(formValues.password || "")) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'La contraseña debe tener mínimo una mayúscula, un carácter especial y un número',
+      });
+      return;
+    }
+  
+    let newUser: User = new User(
+      "",
+      formValues.name!,
+      formValues.lastName!,
+      "",
+      formValues.age!,
+      formValues.email!,
+      formValues.password!,
+      "user"
+    );
+  
+  
+    this.userService.createUser(newUser, file).subscribe(
+      res => {
+        if (res) {
+          Swal.fire({
+            icon: 'success',
+            title: '¡Muy bien!',
+            text: res.message,
+          });
+        }
+        window.location.reload()
+      },
+      err => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error: ' + err.status,
+          text: err.error.error,
+        });
+      }
+    );
+  }
+  
+
+  logIn(){
+    const formValues = this.formLogin.value
+
+    if(!this.formLogin.valid){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Por favor, completa todos los campos correctamente',
+      });
+      return;
+    }
+
+    let userLogin: UserLogin = {
+      email: formValues.email!,
+      password: formValues.password!
+    }
+
+    this.authService.auth(userLogin).subscribe(res => {
+      if(res){
+        let userLocalStorage = res.data as UserLocalStorage
+        localStorage.setItem('user',JSON.stringify(userLocalStorage))
+        Swal.fire({
+          icon: 'success',
+          title: '¡Muy bien!',
+          text: res.message,
+        })
+        window.location.replace('/inicio')
+      }
+    }, err => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error: '+ err.status,
+        text: err.error.error,
+      })
+      console.log(err)
+    })
+  }
+
+
+  loginFunction(): void {
+    const loginForm = document.querySelector('.login-form') as HTMLElement;
+    const registerForm = document.querySelector('.register-form') as HTMLElement;
+    const wrapper = document.querySelector('.wrapper') as HTMLElement;
+    const loginTitle = document.querySelector('.title-login') as HTMLElement;
+    const registerTitle = document.querySelector('.title-register') as HTMLElement;
+
+    if (loginForm && registerForm && wrapper && loginTitle && registerTitle) {
+      loginForm.style.left = '50%';
+      loginForm.style.opacity = '1';
+      registerForm.style.left = '150%';
+      registerForm.style.opacity = '0';
+      wrapper.style.height = '500px';
+      loginTitle.style.top = '50%';
+      loginTitle.style.opacity = '1';
+      registerTitle.style.top = '50px';
+      registerTitle.style.opacity = '0';
+    }
+  }
+
+  registerFunction(): void {
+    const loginForm = document.querySelector('.login-form') as HTMLElement;
+    const registerForm = document.querySelector('.register-form') as HTMLElement;
+    const wrapper = document.querySelector('.wrapper') as HTMLElement;
+    const loginTitle = document.querySelector('.title-login') as HTMLElement;
+    const registerTitle = document.querySelector('.title-register') as HTMLElement;
+
+    if (loginForm && registerForm && wrapper && loginTitle && registerTitle) {
+      loginForm.style.left = '-50%';
+      loginForm.style.opacity = '0';
+      registerForm.style.left = '50%';
+      registerForm.style.opacity = '1';
+      wrapper.style.height = '580px';
+      loginTitle.style.top = '-60px';
+      loginTitle.style.opacity = '0';
+      registerTitle.style.top = '50%';
+      registerTitle.style.opacity = '1';
+    }
+  }
+}
