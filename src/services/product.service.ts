9b5600b5f32ecc69db95cea27123e6b62bb28ea4
TheChec <chechoosorio@hotmail.com>
2025-04-07 19:40:38 -0500

    appcomponent y home

---
+++ src/services/product.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product, Review } from '../models/product.model';
+import { Response } from '../models/response.model';
+import { environment } from '../environments/environments';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  readonly apiUrl = environment.Backend + 'products'
+
+  constructor(private http: HttpClient) { }
+
+  postProduct(product: Product, file: File){
+    const formData = new FormData()
+
+    formData.append('name', product.name)
+    formData.append('description', product.description)
+    formData.append('category', product.category)
+    formData.append('img', file?.name)
+    formData.append('file', file)
+
+    return this.http.post<Response<Product>>(this.apiUrl + '/', formData )
+  }
+
+  getProduct(id: string){
+    return this.http.get<Response<Product>>(this.apiUrl + `/${id}`)
+  }
+
+  getProducts(){
+    return this.http.get<Response<Product[]>>(this.apiUrl + `/`)
+  }
+  
+  deleteProduct(id: string){
+    return this.http.delete<Response<Product>>(this.apiUrl + `/${id}`)
+  }
+
+  putReview(id: string, review: Review){
+    return this.http.put<Response<Product>>(this.apiUrl + `/${id}`, review)
+  }
+
+
+}
+
+
+
+
